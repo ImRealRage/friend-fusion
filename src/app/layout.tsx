3ad23e9adcb94b8772b5564cf4bf8d9d8693a8b2
Kanishk Shukla <main.shubhshukla@gmail.com>
2024-07-19 12:13:09 +0530

    top loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ClerkProvider } from "@clerk/nextjs";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,17 +19,25 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="w-full bg-white px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <Navbar />
-        {/* 2024-07-08 20:34:04 */}
-        </div>
-        <div className="bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        {children}
-        </div>
-      </body>
-    </html>
+      <html lang="en">
+        <body className={inter.className}>
+          <NextTopLoader
+            color="linear-gradient(90deg, hsla(127, 81%, 33%, 1) 0%, hsla(183, 85%, 41%, 1) 100%);"
+            speed={1000}
+            crawlSpeed={1000}
+            height={4.5}
+            initialPosition={0.2}
+            easing="ease-out"
+          />
+          <div className="w-full bg-white px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+            <Navbar />
+            {/* 2024-07-08 20:34:04 */}
+          </div>
+          <div className="bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+            {children}
+          </div>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
